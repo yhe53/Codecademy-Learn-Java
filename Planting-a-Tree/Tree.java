7e80f2e6d6e9a2d9badb9fa30205046c8c54d5f5
yhe53 <yueninghe53@gmail.com>
2023-06-23 23:35:41 -0400

    Add project 'Planting a Tree'

---
+++ Planting-a-Tree/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // We're going to introduce ourselves and print a tree to the screen.
+    System.out.println("Hey there, I'm Leah and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("* ** *");
+    System.out.println("******");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  ");
+  }
+}
