f5b14f6cc73f18886c0e84ff561648faaf0f9a20
yhe53 <yueninghe53@gmail.com>
2023-06-24 00:51:52 -0400

    Add project 'Mad Libs'

---
+++ Mad-Libs/MadLibs.java
@@ -0,0 +1,28 @@
+public class MadLibs {
+  /*
+  This program generates a mad libbed story.
+  Author: Laura
+  Date: 2/19/2049
+  */
+  	public static void main(String[] args){
+      String name1 = "Leah";
+      String adjective1 = "ageotropic";
+      String adjective2 = "quidsworth";
+      String noun1 = "quatrefoil";
+      String noun2 = "palfrey";
+      String verb1 = "intenerate";
+      String noun3 = "mvule";
+      String noun4 = "farandine";
+      String adjective3 = "scared";
+      String name2 = "Maggie";
+      String place1 = "Olk";
+      String noun5 = "eulachon";
+      String number = "2998";
+      String noun6 = "loris";
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be an "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
+    }       
+}
